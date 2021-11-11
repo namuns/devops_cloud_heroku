1e9f2c356794ee8447440c65a76a989216cd8db2
namuns <skymsn214@gmail.com>
2021-11-11 11:40:06 +0900

    초간단 사이트 만들기

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, Heroku"
+    return render_template("index.html")
+
+@app.route("/profile")
+def profile():
+    #DB Access.가능.
+    like_foods = ["스테이크", "전골", "고기", "쌀국수", "자장면",]
+    return render_template("profile.html", like_foods=like_foods)
+
+@app.route("/posts")
+def post_list():
+    return render_template("post_list.html")
+
